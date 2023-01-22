5e44195158917d6d1be92e7cb141ddaba92a8d8c
atuhin <atuhinbd@gmail.com>
2023-01-22 13:45:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const  app =  express();
+const port = 6000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running ${port}`)
+});
+
+// const express = require('express')
+// const app = express()
+// const port = 4000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
